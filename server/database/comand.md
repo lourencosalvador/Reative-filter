46eb2fc67f50430d4bf072246e3e911767c88c2e
lourencosalvador <lourencocardoso007@gmail.com>
2024-07-28 23:43:54 +0100

    first commit

---
+++ server/database/comand.md
@@ -0,0 +1,3 @@
+## Comando para rodar a database 
+
+-> npx json-server --watch server/database/bd.json
---
+++ src/app/_components/filter-comps.tsx
@@ -0,0 +1,52 @@
+'use client'
+import React, { useState, useEffect } from 'react';
+import { from } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+import axios from 'axios';
+
+interface Item {
+  id: number;
+  [key: string]: any;
+}
+
+export function FilterCompos() {
+  const [item, setItem] = useState<Item | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/produtos');
+        const result = await response.data
+        // console.log(result)
+        const data: Item[] = response.data; 
+        const observable = from(data);
+        observable
+          .pipe(
+            filter((item: Item) => item.id === 9), 
+            map((filteredItem: Item) => ({
+              ...filteredItem,
+              additionalProperty: 'Value',
+            }))
+          )
+          .subscribe(setItem);
+      } catch (error) {
+        console.error('Erro ao buscar dados:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {item ? (
+        <div>
+          <h1>Item filtrado:</h1>
+          <pre>{JSON.stringify(item, null, 2)}</pre>
+        </div>
+      ) : (
+        <p>Carregando...</p>
+      )}
+    </div>
+  );
+}
